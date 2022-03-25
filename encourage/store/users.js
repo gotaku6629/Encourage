0add45e5acb599fb0b4781a1988f2fa1c6619867
gotaku6629 <t.goto.760@stn.nitech.ac.jp>
2022-03-25 18:38:58 +0900

    commit

---
+++ encourage/store/users.js
@@ -0,0 +1,22 @@
+import { firestoreAction } from 'vuexfire'
+
+export const state = () => ({
+  items: [],
+})
+
+export const getters = {
+  all: state => {
+    return state.items
+  },
+  byId: (state) => (movieId) => {
+    const res = state.items.filter(movie => movie.id === movieId)
+    return res.length > 0 ? res[0] : null
+  },
+}
+
+export const actions = {
+  bind: firestoreAction(function ({ bindFirestoreRef }) {
+    // return the promise returned by `bindFirestoreRef`
+    return bindFirestoreRef('items', this.$fire.firestore.collection('users'))
+  }),
+}
