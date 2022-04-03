8307e075fd6c8686f4f651969cb9c24f4b5fdf8c
yama30120 <mtb0x0010@gmail.com>
2022-04-03 13:52:14 +0900

    ランキングページとサンプルユーザ作成のリファクタリング

---
+++ encourage/components/CreatSampleUsersButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-btn color="secondary" @click="create">create sample users</v-btn>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import sampleUsers from '../assets/SampleUsers.json'
+
+export default {
+  data() {
+    return {
+      sampleUsers
+    }
+  },
+  mounted() {
+  },
+  methods: {
+    ...mapActions({ createSampleUsers: 'users/sampleSet' }),
+    async create() {
+      await this.createSampleUsers(this.sampleUsers)
+    },
+  },
+}
+</script>
+
+<style></style>
